503cbbfbb02f7062fa2ce2f430d503c87d3b68b5
yanchangxing <ycx1991@live.com>
2023-09-28 18:09:23 +0800

    位置搜索

---
+++ src/components/placeSearch/index.vue
@@ -0,0 +1,103 @@
+<script setup>
+import { ref, onMounted, reactive } from 'vue'
+import { Search, Icon } from 'vant'
+import { debounce } from '@/utils/util.js'
+
+const emit = defineEmits(['selectPlace'])
+const props = defineProps({
+    placeholder: {
+        type: String,
+        default: "请输入要搜索的位置"
+    }
+})
+const searchValue = ref('')
+let valueList = ref([])
+const placeSearch = new AMap.PlaceSearch({
+    pageSize: 20, // 单页显示结果条数
+    pageIndex: 1, // 页码
+    city: "深圳", // 兴趣点城市
+    citylimit: true  //是否强制限制在设置的城市内搜索
+})
+
+const valueChange = debounce((val) => {
+    if (val) {
+        console.log(val)
+        placeSearch.search(val, function (state, result) {
+            console.log(result)
+            if (result?.info === 'OK') {
+                valueList.value = result.poiList.pois
+            }
+        })
+    } else {
+        valueList.value = []
+    }
+}, 500)
+function select(val) {
+    searchValue.value = ''
+    valueList.value = []
+    emit('selectPlace', val)
+}
+</script>
+
+<template>
+    <div class="place-search">
+        <Search v-model="searchValue" @update:model-value="valueChange" :placeholder="props.placeholder" />
+        <div class="list" v-show="valueList.length > 0">
+            <div class="list-item" v-for="item in valueList" :key="item.id" @click="select(item)">
+                <span class="text">{{ item.name }}</span>
+                <Icon name="arrow" color="#898989" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="less" scoped>
+@import '@/assets/style/variable.less';
+
+.place-search {
+    position: relative;
+    --van-search-padding: 0;
+    --van-search-content-background: #fff;
+    --van-radius-sm: 6px;
+
+    /deep/.van-search {
+        border-radius: 6px;
+    }
+    /deep/ .van-cell{
+        margin-bottom: 0;
+    }
+    /deep/ .van-field__body {
+        border: none;
+        margin-bottom: 0;
+    }
+}
+
+.list {
+    margin-top: 2px;
+    max-height: 260px;
+    overflow-y: auto;
+    background: #fff;
+    .wrapper-box();
+
+    .list-item {
+        width: 100%;
+        padding: 10px 0;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .text {
+            flex-grow: 1;
+            width: 100px;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            word-break: break-all;
+            white-space: nowrap;
+        }
+
+        i {
+            flex-shrink: 0;
+        }
+    }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,33 +1,76 @@
 
 <script setup>
+import { ref } from 'vue'
 import useMap from '@/hoc/useMap';
-import {Icon} from 'vant'
+import { Icon, Search } from 'vant'
 import navTabs from '@/components/navTabs/index.vue'
+import placeSearch from '@/components/placeSearch/index.vue'
 
 const { map } = useMap('map-container')
+
+let marker = null
+
 function getLocation() {
+  map.value.remove(marker)
   map.value.setCenter([114.060842, 22.54459], false, 150)
+}
+function selectPlace(val) {
+  setMarker(val)
+  map.value.setCenter(val.location, false, 150)
+}
+function setMarker(item) {
+  if (marker) {
+    marker.setPosition(item.location)
+    marker.setLabel({
+      content: item.name,
+      direction: 'top',
+      offset: [0, -4]
+    })
+    map.value.add(marker)
+  } else {
+    marker = new AMap.Marker({
+      label:{
+        content: item.name,
+        direction: 'top',
+        offset: [0, -4]
+      },
+      map: map.value,
+      position: item.location
+    })
+    map.value.add(marker)
+  }
 }
 </script>
 
 <template>
-  <div class="map-home">
-    <div class="toolWrapper">
-      <div class="tool"><Icon name="/svg/fold.svg" size="24" /></div>
-      <div class="tool" @click="getLocation"><Icon name="/svg/location.svg" size="24" /></div>
+  <div class="tab-page map-home">
+    <div class="search">
+      <placeSearch @selectPlace="selectPlace" />
     </div>
-    <div id="map-container" class="map-wrapper"></div>
+    <div class="toolWrapper">
+      <div class="tool">
+        <Icon name="/svg/fold.svg" size="24" />
+      </div>
+      <div class="tool" @click="getLocation">
+        <Icon name="/svg/location.svg" size="24" />
+      </div>
+    </div>
+    <div id="map-container" class="main map-wrapper"></div>
     <navTabs active="home" />
   </div>
 </template>
 
 <style lang="less" scoped>
 .map-home {
-  width: 100%;
-  height: 100vh;
   position: relative;
-  display: flex;
-  flex-direction: column;
+
+  .search {
+    position: absolute;
+    top: 10px;
+    left: 15px;
+    right: 15px;
+    z-index: 2;
+  }
 
   .toolWrapper {
     position: absolute;
@@ -53,7 +96,6 @@ function getLocation() {
   .map-wrapper {
     width: 100%;
     height: 100px;
-    flex-grow: 1;
   }
 }
 </style>
