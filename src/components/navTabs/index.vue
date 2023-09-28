e507c66d07439e9516d90eed0f6c1749e7cf6ac4
yanchangxing <ycx1991@live.com>
2023-09-28 12:05:20 +0800

    导航菜单修改

--- src/components/navTabs/index.vue
+++ src/components/navTabs/index.vue
@@ -1,14 +1,16 @@
 <script setup>
-import { ref } from 'vue'
+import { defineProps } from 'vue'
 import { Tabbar, TabbarItem } from 'vant';
-const acvtive = ref('home')
+const props = defineProps({
+    active: String
+})
 </script>
 
 <template>
-    <Tabbar v-model="acvtive" fixed placeholder>
-        <TabbarItem name="home" icon="home-o" to="/home">首页</TabbarItem>
+    <Tabbar v-model="props.active" fixed placeholder>
+        <TabbarItem name="home" icon="wap-home-o" to="/home">首页</TabbarItem>
         <TabbarItem name="apply" icon="list-switching" to="apply">申报</TabbarItem>
         <TabbarItem name="history" icon="orders-o" to="/history">订单</TabbarItem>
-        <TabbarItem name="me" icon="user-o" to="/me">我的</TabbarItem>
+        <TabbarItem name="user" icon="user-o" to="/user">我的</TabbarItem>
     </Tabbar>
 </template>
